e62c57e8fd5fccfd3f6284f5db1dac81e7e6b613
Augusto Ayala <50784512+AugustoAyala@users.noreply.github.com>
2020-05-18 20:05:40 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,61 +1,54 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
 const fetch = require('node-fetch');
 const Bull = require('bull');
 const myFirstQueue = new Bull('my-first-queue', 'redis://127.0.0.1:6379');
 
-
-router.post('/url', async (req, res)=>{
- 
-  res.json({"status": "se recibio con exito"});
-  
+router.post('/url', async (req, res) => {
+    res.json({ status: 'se recibio con exito' });
 });
-router.post('/externo', async (req, res)=>{
- 
-  res.json(req.body.content.content.phone);
-  
+router.post('/externo', async (req, res) => {
+    res.json(req.body.content.content.phone);
 });
 
 router.get('/api/:firstname/:lastname/:email/:zipcode/:phone', (req, res) => {
-    if(Number(req.params.zipcode) == 08456){
+    if (Number(req.params.zipcode) == 08456) {
         var code = 'CO1234';
-    }else if (Number(req.params.zipcode) == 09789) {
+    } else if (Number(req.params.zipcode) == 09789) {
         var code = 'CO5678';
     } else {
         var code = 'ES3456';
-    };
+    }
     var data = {
-        
-            "firstname": req.params.firstname,
-            "lastname": req.params.lastname,
-            "email": req.params.email,
-            "zipcode": code,
-            "phone": req.params.phone
-        
-    }; 
-    //Cambiar por la url que corresponda en este caso para pruba local llama a esta ruta 
-var url = 'http://localhost:3000/url';
-//var url = 'https://beeceptor.com';
-fetch(url, {
-  method: 'POST', 
-  body: JSON.stringify(data), 
-  headers:{
-    'Content-Type': 'application/json'
-  }
-}).then(checkStatus)
-  .catch(err => console.error(err));
-  
- async function checkStatus(res) {
-    if (res.ok) { 
-        console.log(res.ok);
-        const job = await myFirstQueue.add({
-        content: data
-        });
-        console.log(job.data.content);
-    } 
-}
-res.json(data);
+        firstname: req.params.firstname,
+        lastname: req.params.lastname,
+        email: req.params.email,
+        zipcode: code,
+        phone: req.params.phone,
+    };
+    //Cambiar por la url que corresponda en este caso para pruba local llama a esta ruta
+    var url = 'http://localhost:3000/url';
+    //var url = 'https://beeceptor.com';
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(checkStatus)
+        .catch((err) => console.error(err));
+
+    async function checkStatus(res) {
+        if (res.ok) {
+            console.log(res.ok);
+            const job = await myFirstQueue.add({
+                content: data,
+            });
+            console.log(job.data.content);
+        }
+    }
+    res.json(data);
 });
 
 module.exports = router;  
-
