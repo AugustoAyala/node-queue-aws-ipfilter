c5bccce1dacfa418d8e89dddf4a7acad379835f1
Augusto Ayala <50784512+AugustoAyala@users.noreply.github.com>
2020-05-18 20:06:38 -0300

    Update aws.js

--- src/delivery/aws.js
+++ src/delivery/aws.js
@@ -1,34 +1,34 @@
 var AWS = require('aws-sdk');
 
 module.exports.send = (from, to, subject, bodyHtml, bodyText) => {
-    AWS.config.update({region: 'sa-east-1'});
+    AWS.config.update({ region: 'sa-east-1' });
 
     return new Promise((resolve, reject) => {
-        let ses = new aws.SES({apiVersion: '2010-12-01'});
+        let ses = new aws.SES({ apiVersion: '2010-12-01' });
 
         let params = {
             Source: from,
             Destination: {
-                ToAddresses: to
+                ToAddresses: to,
             },
             Message: {
                 Subject: {
-                    Data: subject
+                    Data: subject,
                 },
                 Body: {
                     Html: {
-                        Data: bodyHtml
+                        Data: bodyHtml,
                     },
                     Text: {
-                        Data: bodyText
-                    }
-                }
-            }
+                        Data: bodyText,
+                    },
+                },
+            },
         };
 
         ses.sendEmail(params, (error, data) => {
             console.log(error);
-            (error) ? reject(error) : resolve(data);
-        })
+            error ? reject(error) : resolve(data);
+        });
     });
-}
+};
